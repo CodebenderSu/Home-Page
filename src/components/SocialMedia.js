34e5fa1cb035f3f86081db9bd882f19768e47a1e
Su <codebendersu@gmail.com>
2018-05-29 18:16:21 -0700

    [Refactor] Toolbar changes
    Header and Toolbar now have a hard, pixel height
    Flex properties reconfigured
    Added Facebook button primarily for comparing to the Google button, but also as feature
    Fixed positioning of Google search box

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import * as Google from '../utils/images/google.png';
+import * as Facebook from '../utils/images/facebook.png';
 
 class SocialMedia extends Component {
   render() {
     return (
       <span className="social-media">
         <div className="google-search">
-        <a href="https://www.google.com/search"><img src={Google} /></a>
-        <div className="google-search-content">
-          <form action="https://www.google.com/search" method="get" target="_blank">
-            <input type="hidden" value="" />
-            <input autocomplete="on" name="q" placeholder="Search Google" required="required"  type="text" />
-            <button type="submit">Search</button>
-          </form>
+          <a href="https://www.google.com/search">
+            <img src={Google} alt="Google" title="Search Google" />
+          </a>
+          <div className="google-search-content">
+            <form action="https://www.google.com/search" method="get" target="_blank">
+              <input type="hidden" value="" />
+              <input autocomplete="on" name="q" placeholder="Search Google"
+                    required="required"  type="text" title="Google it!" />
+              <button type="submit">Search</button>
+            </form>
+          </div>
         </div>
+        <div className="facebook">
+          <a href="https://www.facebook.com/">
+            <img src={Facebook} alt="Facebook" title="Facebook" />
+          </a>
         </div>
       </span>
     );
